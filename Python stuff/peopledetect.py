444c3d8a0ebb47c2ecd6654086df23226903df1f
unknown <Rigs@gersi.(none)>
2011-12-04 16:51:21 -0500

    peopledetect.py spits out coordinates

--- Python stuff/peopledetect.py
+++ Python stuff/peopledetect.py
@@ -1,6 +1,10 @@
 import sys
 from cv import *
 import Image
+
+
+filename = "coordinates.txt"
+
 ##import cv
 
 def inside(r, q):
@@ -45,9 +49,11 @@ while True:
 ##    Smooth( img, img, CV_GAUSSIAN, 11, 11 )
 
     img2 = CreateMat(240, 320, CV_8UC3)
+    img3 = CreateMat(120,160, CV_8UC3)
 
 
     PyrDown(img, img2)
+    PyrDown(img2, img3)
 
 ##    data = np.asarray(img2)
 ##    blue, green, red = data.T
@@ -60,7 +66,7 @@ while True:
 
     
     found = list(HOGDetectMultiScale(img2, storage, win_stride=(8,8),
-        padding=(32,32), scale=.8, group_threshold=1))
+        padding=(32,32), scale=1.05, group_threshold=2))
     found_filtered = []
     for r in found:
         insidef = False
@@ -72,11 +78,48 @@ while True:
             found_filtered.append(r)
     for r in found_filtered:
         (rx, ry), (rw, rh) = r
+##        print(rx, ry, rw, rh)
+        target = open(filename, 'w')
+        target.write(str(rx))
+        target.write(" ")
+        target.write(str(ry))
+        target.write(" ")
+        target.write(str(rw))
+        target.write(" ")
+        target.write(str(rh))
+        target.write("\n")
+        target.close()
         tl = (rx + int(rw*0.1), ry + int(rh*0.07))
         br = (rx + int(rw*0.9), ry + int(rh*0.87))
-        Rectangle(img2, tl, br, (0, 255, 0), 3)
+        Rectangle(img2, tl, br, (0, 255, 0), 1)
             
     ShowImage("people detection demo", img2)
+
+##    def run(self):
+##        hist = cv.CreateHist([180], cv.CV_HIST_ARRAY, [(0,180)], 1 )
+##        backproject_mode = False
+##        while True:
+##            frame = cv.QueryFrame(Rectangle)
+##
+##            # Convert to HSV and keep the hue
+##            hsv = cv.CreateImage(cv.GetSize(frame), 8, 3)
+##            cv.CvtColor(frame, hsv, cv.CV_BGR2HSV)
+##            self.hue = cv.CreateImage(cv.GetSize(frame), 8, 1)
+##            cv.Split(hsv, self.hue, None, None, None)
+##
+##            # Compute back projection
+##            backproject = cv.CreateImage(cv.GetSize(frame), 8, 1)
+##
+##            # Run the cam-shift
+##            cv.CalcArrBackProject( [self.hue], backproject, hist )
+##            if self.track_window and is_rect_nonzero(self.track_window):
+##                crit = ( cv.CV_TERMCRIT_EPS | cv.CV_TERMCRIT_ITER, 10, 1)
+##                (iters, (area, value, rect), track_box) = cv.CamShift(backproject, self.track_window, crit)
+##                self.track_window = rect
+
+
+
+    
 ##    c = WaitKey(0)
     ##if c == ord('q'):
     ##    break
